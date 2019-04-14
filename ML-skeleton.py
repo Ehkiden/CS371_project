de7936cb40ee31aa1560c2e00ebd8c1021e8afc7
David Mercado <dme236@g.uky.edu>
2019-04-14 15:44:59 -0400

    got all ML models working

--- ML-skeleton.py
+++ ML-skeleton.py
@@ -2,11 +2,16 @@ import pandas as pd     #library
 import numpy as np      #library
 import matplotlib.pyplot as plt   #library
 import csv
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.svm import SVC             #machine learning model
-from sklearn.svm import LinearSVC       #machine learning model
-from sklearn import tree                #machine learning model
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.svm import SVC                                     #machine learning model
+from sklearn.svm import LinearSVC                               #machine learning model
+from sklearn import tree                                        #machine learning model
+from sklearn.neural_network import MLPClassifier                #machine learning model
 
 
 try:
@@ -40,19 +45,18 @@ labelFileDL = 4
 
 #Will run the machine learning model 10 times (cross validation)
 TreeAccScores = []
-mlpAccScores = []
-svcAccScores = []
-
 TreePrecsionScores = []
-mlpPrecsionScores = []
-svcPrecsionScores = []
-
 TreeRecallScores = []
-mlpRecallScores = []
-svcRecallScores = []
-
 TreeF1Scores = []
+
+mlpAccScores = []
+mlpPrecsionScores = []
+mlpRecallScores = []
 mlpF1Scores = []
+
+svcAccScores = []
+svcPrecsionScores = []
+svcRecallScores = []
 svcF1Scores = []
 
 for i in range(0, 10):
@@ -63,41 +67,84 @@ for i in range(0, 10):
     #MACHINE LEARNING MODEL 1---------------------------------------------------
     #Decision Trees
     #Reference: https://scikit-learn.org/stable/modules/tree.html#tree
+    #Reference: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.accuracy_score.html
+    #Reference: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.precision_score.html#sklearn.metrics.precision_score
+    #Reference: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.recall_score.html#sklearn.metrics.recall_score
+    #Reference: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html#sklearn.metrics.f1_score
     
-    clf = tree.DecisionTreeClassifier()            #initialize the Decision Tree
-    clf.fit(X_train, y_train)                      #train the model with training sets
-    y_pred_class = clf.predict(X_test)             #test the model
-    TreeAccScores.append(accuracy_score(y_test, y_pred_class))
-#TODO
-#Precision
-#recall
-#F1 score
+    clf = tree.DecisionTreeClassifier()                                         #initialize the Decision Tree
+    clf.fit(X_train, y_train)                                                   #Build a decision tree classifier from the training set (X, y).
+    y_pred_class = clf.predict(X_test)                                          #Predict class or regression value for X. Returns the predicted classes/values
+    print('y_pred_class is: ', y_pred_class)
 
-    #results = clf.score(X_test, y_test)
-    #print(empty(results))
-    #print(bool(results))
-    #print(item(results))
-    #print(all(results))
+    acc = accuracy_score(y_test, y_pred_class)
+    print('accScores is: ', acc)
+    result = clf.score(X_test, y_test)
+    print('results is: ', result)
+    
+    TreeAccScores.append(accuracy_score(y_test, y_pred_class))                              #accuracy scores
+    TreePrecsionScores.append(precision_score(y_test, y_pred_class, average='weighted'))    #precsion scores
+    TreeRecallScores.append(recall_score(y_test, y_pred_class, average='weighted'))         #recall scores
+    TreeF1Scores.append(f1_score(y_test, y_pred_class, average='weighted'))                 #f1 scores
     
     #MACHINE LEARNING MODEL 2---------------------------------------------------
     # Neural network (MultiPerceptron Classifier)
-    # clf = MLPClassifier()
-    # clf.fit(X_train, y_train)
+
+    clf = MLPClassifier()
+    clf.fit(X_train, y_train)
+
+    y_pred_class = clf.predict(X_test)                                          #Predict class or regression value for X. Returns the predicted classes/values
+    print('y_pred_class is: ', y_pred_class)
+    
+    acc = accuracy_score(y_test, y_pred_class)
+    print('accScores is: ', acc)
+    result = clf.score(X_test, y_test)
+    print('results is: ', result)
+    
+    mlpAccScores.append(accuracy_score(y_test, y_pred_class))                              #accuracy scores
+    mlpPrecsionScores.append(precision_score(y_test, y_pred_class, average='weighted'))    #precsion scores
+    mlpRecallScores.append(recall_score(y_test, y_pred_class, average='weighted'))         #recall scores
+    mlpF1Scores.append(f1_score(y_test, y_pred_class, average='weighted'))                 #f1 scores
 
     #MACHINE LEARNING MODEL 3---------------------------------------------------
     #SVM's
-    # clf = SVC(gamma='auto')     #SVC USE THIS
-    # clf = LinearSVC()  #Linear SVC
-    # clf.fit(X_train, y_train)
+    clf = SVC(gamma='auto')     #SVC USE THIS
+    clf = LinearSVC()  #Linear SVC
+    clf.fit(X_train, y_train)
+
+    y_pred_class = clf.predict(X_test)                                          #Predict class or regression value for X. Returns the predicted classes/values
+    print('y_pred_class is: ', y_pred_class)
+    
+    acc = accuracy_score(y_test, y_pred_class)
+    print('accScores is: ', acc)
+    result = clf.score(X_test, y_test)
+    print('results is: ', result)
+    
+    svcAccScores.append(accuracy_score(y_test, y_pred_class))                              #accuracy scores
+    svcPrecsionScores.append(precision_score(y_test, y_pred_class, average='weighted'))    #precsion scores
+    svcRecallScores.append(recall_score(y_test, y_pred_class, average='weighted'))         #recall scores
+    svcF1Scores.append(f1_score(y_test, y_pred_class, average='weighted'))                 #f1 scores
 
 #resultsAcc = (acc_scores/10)
 #print('avg score', resultsAcc)
 #===============================================================================
 #EVALUATION PORTION-------------------------------------------------------------
     #here you are supposed to calculate the evaluation measures indicated in the project proposal (accuracy, F-score etc)
-result = clf.score(X_test, y_test)  #accuracy score
-print(result)
 
+print('Complete TreeAccScores array: ', TreeAccScores)
+print('Complete TreePrecsionScores array: ', TreePrecsionScores)
+print('Complete TreeRecallScores array: ', TreeRecallScores)
+print('Complete TreeF1Scores array: ', TreeF1Scores)
+
+print('Complete mplAccScores array: ', mlpAccScores)
+print('Complete mplPrecsionScores array: ', mlpPrecsionScores)
+print('Complete mplRecallScores array: ', mlpRecallScores)
+print('Complete mplF1Scores array: ', mlpF1Scores)
+
+print('Complete svcAccScores array: ', svcAccScores)
+print('Complete svcPrecsionScores array: ', svcPrecsionScores)
+print('Complete svcRecallScores array: ', svcRecallScores)
+print('Complete svcF1Scores array: ', svcF1Scores)
 
 #===============================================================================
 #Graph results for machine learning models and features for each label
@@ -111,7 +158,7 @@ f6 = 'DestBytes'
 f7 = 'DurrTime'
 
 #sample data
-sampleDATA = (3,4,1,3,4,6,6,7,5,4)
+#sampleDATA = (3,4,1,3,4,6,6,7,5,4)
 np.random.seed(19680801)
 
 n_bins = 10
@@ -131,53 +178,52 @@ ax0.set_title('Feature Details')
 ax0.set_ylabel('Occurrences')
 ax0.set_xlabel('Features')
 
-MLP = (3,4,1,3,4,6,6,7,5,4)
-SVC = (9,6,4,7,5,3,5,7,8,6)
 #Accuracy-----------------------------------------------------------------------
-treeBar = ax1.bar(ind, TreeAccScores, width)
-mlpBar = ax1.bar(ind, MLP, width, bottom=TreeAccScores)
-svcBar = ax1.bar(ind, SVC, width, bottom=MLP)
+treeBarAcc = ax1.bar(ind, TreeAccScores, width)
+mlpBarAcc = ax1.bar(ind, mlpAccScores, width, bottom=TreeAccScores)
+svcBarAcc = ax1.bar(ind, svcAccScores, width, bottom=mlpAccScores)
 
 ax1.set_title('Accuracy')
 ax1.set_ylabel('Accuracy Score')
 ax1.set_xticks(ind, ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
 ax1.set_xlabel('Executions')
-ax1.legend((treeBar[0], mlpBar[0], svcBar[0]), ('Decision Tree', 'Netural Network', 'Support Vector Machine'))
+ax1.legend((treeBarAcc[0], mlpBarAcc[0], svcBarAcc[0]), ('Decision Tree', 'Netural Network', 'Support Vector Machine'))
 
 #Precision----------------------------------------------------------------------
-treeBar = ax2.bar(ind, TreeAccScores, width)
-mlpBar = ax2.bar(ind, MLP, width, bottom=TreeAccScores)
-svcBar = ax2.bar(ind, SVC, width, bottom=MLP)
+treeBarPre = ax2.bar(ind, TreePrecsionScores, width)
+mlpBarPre = ax2.bar(ind, mlpPrecsionScores, width, bottom=TreePrecsionScores)
+svcBarPre = ax2.bar(ind, svcPrecsionScores, width, bottom=mlpPrecsionScores)
 
 ax2.set_title('Precision')
 ax2.set_ylabel('Precision Score')
 ax2.set_xticks(ind, ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
 ax2.set_xlabel('Executions')
-ax2.legend((treeBar[0], mlpBar[0], svcBar[0]), ('Decision Tree', 'Netural Network', 'Support Vector Machine'))
+ax2.legend((treeBarPre[0], mlpBarPre[0], svcBarPre[0]), ('Decision Tree', 'Netural Network', 'Support Vector Machine'))
 
 #Recall-------------------------------------------------------------------------
-treeBar = ax3.bar(ind, TreeAccScores, width)
-mlpBar = ax3.bar(ind, MLP, width, bottom=TreeAccScores)
-svcBar = ax3.bar(ind, SVC, width, bottom=MLP)
+treeBarRecall = ax3.bar(ind, TreeRecallScores, width)
+mlpBarRecall = ax3.bar(ind, mlpRecallScores, width, bottom=TreeRecallScores)
+svcBarRecall = ax3.bar(ind, svcRecallScores, width, bottom=mlpRecallScores)
 
 ax3.set_title('Recall')
 ax3.set_ylabel('Recall Score')
 ax3.set_xticks(ind, ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
 ax3.set_xlabel('Executions')
-ax3.legend((treeBar[0], mlpBar[0], svcBar[0]), ('Decision Tree', 'Netural Network', 'Support Vector Machine'))
+ax3.legend((treeBarRecall[0], mlpBarRecall[0], svcBarRecall[0]), ('Decision Tree', 'Netural Network', 'Support Vector Machine'))
 
 #F1-----------------------------------------------------------------------------
-treeBar = ax4.bar(ind, TreeAccScores, width)
-mlpBar = ax4.bar(ind, MLP, width, bottom=TreeAccScores)
-svcBar = ax4.bar(ind, SVC, width, bottom=MLP)
+treeBarF1 = ax4.bar(ind, TreeF1Scores, width)
+mlpBarF1 = ax4.bar(ind, mlpF1Scores, width, bottom=TreeF1Scores)
+svcBarF1 = ax4.bar(ind, svcF1Scores, width, bottom=mlpF1Scores)
 
 ax4.set_title('F1')
 ax4.set_ylabel('F1 Score')
 ax4.set_xticks(ind, ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
 ax4.set_xlabel('Executions')
-ax4.legend((treeBar[0], mlpBar[0], svcBar[0]), ('Decision Tree', 'Netural Network', 'Support Vector Machine'))
+ax4.legend((treeBarF1[0], mlpBarF1[0], svcBarF1[0]), ('Decision Tree', 'Netural Network', 'Support Vector Machine'))
 
-#F1-----------------------------------------------------------------------------
+'''
+#-----------------------------------------------------------------------------
 treeBar = ax5.bar(ind, TreeAccScores, width)
 mlpBar = ax5.bar(ind, MLP, width, bottom=TreeAccScores)
 svcBar = ax5.bar(ind, SVC, width, bottom=MLP)
@@ -187,6 +233,7 @@ ax5.set_ylabel('Accuracy Score')
 ax5.set_xticks(ind, ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
 ax5.set_xlabel('Executions')
 ax5.legend((treeBar[0], mlpBar[0], svcBar[0]), ('Decision Tree', 'Netural Network', 'Support Vector Machine'))
+'''
 '''
 ax2.hist(x, n_bins, histtype='step', stacked=True, fill=False)
 ax2.set_title('stack step (unfilled)')
